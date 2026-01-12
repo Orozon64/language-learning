ebdffd58aeb157c370351cd629c621f5293eaebb
Orozon64 <ignacy.guminiak@tm1.edu.pl>
2026-01-12 11:15:18 +0100

    continued database work

--- src/server.js
+++ src/server.js
@@ -7,4 +7,40 @@ const connection = mysql.createConnection({
   password: 'langauage-learning',
   database: 'my_db'
 })
-connection.connect()
+function getWordsByWordsetId(wordsetId) {
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    let sql = "SELECT * FROM words WHERE Wordset_ID = " + wordsetId +";";
+    let query_result;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      query_result = result;
+    });
+  });
+  return query_result;
+}
+function createWordset(name) {
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    let sql = "INSERT INTO wordsets (Name) VALUES (" + name + ");";
+    let query_result;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+    });
+  });
+
+}
+function createWord(wordsetID, term, definition, example) {
+  con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    let sql = "INSERT INTO words (Wordset_ID, Term, Definition, Example) VALUES (" + wordsetID +"," + term+","+ definition+","+ example+ ");";
+    let query_result;
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+    });
+  });
+
+}
