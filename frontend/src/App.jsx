72bc86f7beec5713d7b7ae7013c968c4fb36aaf4
Orozon64 <ignacy.guminiak@o2.pl>
2026-02-10 20:09:45 +0100

    fixed learn page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,32 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.css';
-function App(props) {
+function App() {
   const [currentPage, setCurrentPage] = useState("Home")
-  //the 3 below are for the form
+  //the 3 below are for the create word form
   const [term, setTerm] = useState("")
   const [definition, setDefinition] = useState("")
   const [example, setExample] = useState("")
 
+  //and this one is for the create wordset form
+  const [wordsetName, setWordsetName] = useState("")
+
   const [wordsets, setWordsets] = useState([])
 
   const [words, setWords] = useState([]) //will have to be separated by wordset
+
+  const CreateWord = (e) =>{
+    e.preventDefault();
+    useEffect(()=>{
+        axios.post("http://localhost:8080/words", {term, definition, example})
+    }, [])
+  };
+  const CreateWordset = (e) =>{
+    e.preventDefault();
+    useEffect(()=>{
+        axios.post("http://localhost:8080/wordsets", {term, definition, example})
+    }, [])
+  }
   useEffect(()=>{
     axios.get("http://localhost:8080/wordsets").then((response) =>{
       setWordsets(response.data)
@@ -31,22 +47,18 @@ function App(props) {
         (e)=>{setCurrentPage(e.target.value)}
       } value={props.siteName} className="btn btn-primary">{props.siteName}</button>
   )
-}
-function NavMenu(){
-  return(
-    <nav>
-            <MenuButton siteName="Home"></MenuButton>
-            <MenuButton siteName="Learn"></MenuButton>
-            <MenuButton siteName="Statistics"></MenuButton>
-    
-    </nav>
-  )
-}
-  
-  function CreateWordset(e){
-    e.preventDefault();
-    
   }
+  function NavMenu(){
+    return(
+      <nav>
+              <MenuButton siteName="Home"></MenuButton>
+              <MenuButton siteName="Learn"></MenuButton>
+              <MenuButton siteName="Statistics"></MenuButton>
+      
+      </nav>
+    )
+  }
+
   function handleChange(e) {
     switch (e.target.id) { //this is either brilliant or stupid, we'll see
       case 'terminput':
@@ -65,25 +77,20 @@ function NavMenu(){
   }
   function startQuiz(e) {
     e.preventDefault();
-
+    //first, make the form disappear, then reuse the code from physics quiz assignment
+    
   }
+  const wordsetOptions = wordsets.map(wordset => <option value={wordset.Name}>{wordset.Name}</option>)
+
   switch (currentPage) { //return the right content based on the pagename (we always return the nav menu, avoid the repetition)
     case "Home":
-      const wordsetOptions = wordsets.map(wordset => <option value={wordset.Name}>{wordset.Name}</option>)
       
       return (
         <>
           <NavMenu/>
           
           <h2>Create a new word</h2>
-          <form onSubmit={
-            (e)=>{
-              e.preventDefault()
-              useEffect(()=>{
-                    axios.post("http://localhost:8080/words", {term, definition, example}).then //THIS IS NOT COMPLETE
-              }, [])
-            }
-          }>
+          <form onSubmit={CreateWord}>
             <div className='form-group'>
               <label htmlFor="wordsetSelect">Wordset:</label>
               <select id='wordsetSelect' className='form-control'>
@@ -114,7 +121,9 @@ function NavMenu(){
           <form onSubmit={CreateWordset}>
             <div className='form-group'>
               <label htmlFor='nameinput'>Name</label>
-              <input id='nameinput' type="text" className='form-control'/>
+              <input id='nameinput' type="text" className='form-control' value={wordsetName} onChange={
+                (e)=>{setWordsetName(e.target.value)}
+              }/>
             </div>
             
             <input type="submit" value="Create wordset" className='form-control' />
@@ -127,11 +136,14 @@ function NavMenu(){
           <NavMenu/>
           {/*the user will select a wordset they want to be tested on, then they'll be given a random definition and a selection of four random words, one will match the definition*/}
           <form onSubmit={startQuiz}>
-            <label htmlFor="wordsetSelectQuiz">Wordset:</label>
-            <select id='wordsetSelectQuiz'>
-              {wordsetOptions}
-            </select> <br />
-            <input type="submit" value="Start" />
+            <div className='form-group'>
+              <label htmlFor="wordsetSelectQuiz">Wordset:</label>
+              <select id='wordsetSelectQuiz' className='form-control'>
+                {wordsetOptions}
+              </select> 
+            </div>
+            
+            <input type="submit" value="Start" className='form-control'/>
           </form>
           <div id='quizDiv'>
 
